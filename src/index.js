0ebd9c5d880e3c8f2a82c2fe207841d6cc72933a
arvynrf <arvyn.bang@gmail.com>
2024-03-05 16:38:41 +0700

    perbaiki env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require("express");
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const app = express();
 const userRoutes = require('./routes/users');
